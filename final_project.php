fe7cbc2a509c17ff14cb2d87d6715ba9c3ab1f35
Kit <mkitmichail@gmail.com>
2024-11-10 09:26:02 -1200

    first commit

--- final_project.php
+++ final_project.php
@@ -1,3 +1,30 @@
 <?php
+session_start();
+include 'config.php';
 
+// Check credentials (assuming POST method)
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $sql);
+
+if ($result && mysqli_num_rows($result) > 0) {
+    $user = mysqli_fetch_assoc($result);
+
+    // Set session variables
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['user_role'] = $user['role']; // 'job-seeker' or 'employer'
+
+    // Redirect based on role
+    if ($user['role'] === 'job-seeker') {
+        header("Location: job-seeker.html");
+        exit();
+    } elseif ($user['role'] === 'employer') {
+        header("Location: employer.html");
+        exit();
+    }
+} else {
+    echo "Invalid credentials";
+}
 ?>
