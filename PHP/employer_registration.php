f27a06fcad3caadfc5517e44aa3fcd9b26d49fd4
Kit <mkitmichail@gmail.com>
2024-11-18 01:42:29 -1200

    fixing css in main

--- PHP/employer_registration.php
+++ PHP/employer_registration.php
@@ -1,17 +1,5 @@
 <?php
-// Database connection
-$servername = "localhost";
-$username = "root"; // Change this to your database username
-$password = ""; // Change this to your database password
-$dbname = "dbjobconnect";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once "../PHP/main_DB.php";
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
