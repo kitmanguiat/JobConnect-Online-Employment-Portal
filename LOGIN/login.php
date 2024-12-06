5d281c302ec101c86a82b02ee7c746fd467a0e5e
Kit <mkitmichail@gmail.com>
2024-12-06 20:07:26 +0800

    Finalizing the project

--- LOGIN/login.php
+++ LOGIN/login.php
@@ -47,6 +47,6 @@
         </div>
     </footer>
 
-    <script src="../JS/login.js"></script>
+    <script src="../JS/main_login.js"></script>
 </body>
 </html>
--- SIGNUP/signup.php
+++ SIGNUP/signup.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JobConnect - Your Gateway to Career Opportunities</title>
     <link rel="stylesheet" href="../CSS/signup.css">
-    <script src="../JS/signup.js"></script>
  
 </head>
 <body>
